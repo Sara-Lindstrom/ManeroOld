e96c75c0d0c74ceee0738bd0610c2497c954c224
sara-lindstrom <sara.maria.lindstrom@hotmail.com>
2023-10-16 15:40:24 +0200

    init

---
+++ Manero/ClientApp/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import './Style.min.css';
+
+export default class App extends Component {
+    static displayName = App.name;
+
+    render() {
+        return (
+            <Routes>
+
+            </Routes>
+        );
+    }
+}
